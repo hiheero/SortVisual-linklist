4f17414849fcd2140889b2a0f4bb7ec5c285b43a
hiheero <31976850+hiheero@users.noreply.github.com>
2019-06-28 16:35:37 +0800

    Update InsertionSort.java

--- src/org/shichuangnet/heero/sortvisual/linklist/sorts/InsertionSort.java
+++ src/org/shichuangnet/heero/sortvisual/linklist/sorts/InsertionSort.java
@@ -24,25 +24,47 @@ public class InsertionSort implements ILinkSort {
     public void startSort(LinklistController lc) {
     
         //  交换节点法
+//         Node p, q, r;
+//         p = lc.head.getNext();
+//         q = p.getNext();
+//         p.setNext(null);  //  断开链表头
+//         p = q;
+//         while (p != null) {
+//             q = p.getNext();
+//             r = lc.head;
+//             while (r.getNext() != null && r.getNext().getValue() <= p.getValue()) {
+//                 LinklistVisualizer.mySleep(0.02);
+//                 r = r.getNext();
+//             }
+//             p.setNext(r.getNext());
+//             r.setNext(p);
+//             p = q;
+//             //  数组逆置时增长sleep时间（排的太快了）
+//             if (!LinklistVisualizer.SHUFFLEMETHOD) {
+//                 LinklistVisualizer.mySleep(1);
+//             }
+//         }
+        //  交换节点法，不断开头结点
         Node p, q, r;
-        p = lc.head.getNext();
-        q = p.getNext();
-        p.setNext(null);  //  断开链表头
-        p = q;
-        while (p != null) {
-            q = p.getNext();
+        p = lc.head.getNext();  //  p始终指向有序区域末尾
+        q = p.getNext();  //  q始终指向无序空间起始
+        while (q != null) {
             r = lc.head;
-            while (r.getNext() != null && r.getNext().getValue() <= p.getValue()) {
-                LinklistVisualizer.mySleep(0.02);
+            while (r != p) {  //  寻找 p 应该插入的位置 
+                if (r.getNext().getValue() > q.getValue())
+                    break;
                 r = r.getNext();
             }
-            p.setNext(r.getNext());
-            r.setNext(p);
-            p = q;
-            //  数组逆置时增长sleep时间（排的太快了）
-            if (!LinklistVisualizer.SHUFFLEMETHOD) {
-                LinklistVisualizer.mySleep(1);
+            if (r == p) {  //  插入的位置是有序空间的末尾
+                p = q;
+                q = q.getNext();
+            } else {
+                p.setNext(q.getNext());
+                q.setNext(r.getNext());
+                r.setNext(q);
+                q = p.getNext();
             }
+            LinklistVisualizer.mySleep(3);
         }
     }
     
